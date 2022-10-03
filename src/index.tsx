366965b27ebba9576744d2fe09a7e30a729e4c02
Joliver Estampador <joliverestampador@gmail.com>
2022-10-04 07:59:15 +0800

    initialized state
    Details about the hidden issue when initializing the state in constructor vs initializing it in property level:
    https://docs.google.com/document/d/1U13Z42pAdi1dc6lzc6v1Yuc_s2szTmTSP7g3I4Bxta4/edit#heading=h.vfi08s7p8ywh

--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,27 @@ interface AppProps {
 }
 
 class App extends React.Component<AppProps> {
+  state = { counter: 0 };
+  constructor(props: AppProps) {
+    super(props);
+  }
+
+  onIncrement = (): void => {
+    this.setState({ counter: this.state.counter + 1 });
+  }
+
+  onDecrement = (): void => {
+    this.setState({ counter: this.state.counter - 1 });
+  }
+
   render() {
-    return <div>{this.props.color}</div>;
+    return (
+      <div>
+        <button onClick={this.onIncrement}>Increnment</button>
+        <button onClick={this.onDecrement}>Decrement</button>
+        {this.state.counter}
+      </div>
+    )
   }
 }
 
