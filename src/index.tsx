5c86fc4d8c08839a7914c22995b2a1f49db3c03c
Joliver Estampador <joliverestampador@gmail.com>
2022-10-04 08:11:47 +0800

    setting up redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { App } from './components/App';
+import { reducers } from './reducers';
+
+const store = createStore(reducers, applyMiddleware(thunk));
 
 ReactDOM.render(
-  <App color="red" />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.querySelector('#root')
 )
