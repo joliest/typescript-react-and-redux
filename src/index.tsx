b39d1371683b65921aac437e492891e972a53a5b
Joliver Estampador <joliverestampador@gmail.com>
2022-10-04 08:05:42 +0800

    setting up redux (deleted some codes)

--- src/index.tsx
+++ src/index.tsx
@@ -1,35 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-interface AppProps {
-  color?: string;
-}
-
-class App extends React.Component<AppProps> {
-  state = { counter: 0 };
-  constructor(props: AppProps) {
-    super(props);
-  }
-
-  onIncrement = (): void => {
-    this.setState({ counter: this.state.counter + 1 });
-  }
-
-  onDecrement = (): void => {
-    this.setState({ counter: this.state.counter - 1 });
-  }
-
-  render() {
-    return (
-      <div>
-        <button onClick={this.onIncrement}>Increnment</button>
-        <button onClick={this.onDecrement}>Decrement</button>
-        {this.state.counter}
-      </div>
-    )
-  }
-}
-
 ReactDOM.render(
   <App color="red" />,
   document.querySelector('#root')
